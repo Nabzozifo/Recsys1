046b971526a1de9451340f43637b38d20efb854d
Mouhamed Naby Ndiaye <50000511+Nabzozifo@users.noreply.github.com>
2020-06-10 23:32:56 +0100

    Update slateq2.py

--- gym_Recsys1/slateq2.py
+++ gym_Recsys1/slateq2.py
@@ -131,22 +131,22 @@ class RecomRunner:
 
 	def _choose_action(self, state):
 		if random.random() < self._eps:
-			return random.randint(0, self._model.num_actions - 1)
+			return random.randint(0, self._model._num_actions - 1)
 		else:
 			return np.argmax(self._model.predict_one(state, self._sess))
 	
 	def _replay(self):
 		batch = self._memory.sample(self._model.batch_size)
 		states = np.array([val[0] for val in batch])
-		next_states = np.array([(np.zeros(self._model.num_states)
+		next_states = np.array([(np.zeros(self._model._num_states)
 								if val[3] is None else val[3]) for val in batch])
 		# predict Q(s,a) given the batch of states
 		q_s_a = self._model.predict_batch(states, self._sess)
 		# predict Q(s',a') - so that we can do gamma * max(Q(s'a')) below
 		q_s_a_d = self._model.predict_batch(next_states, self._sess)
 		# setup training arrays
-		x = np.zeros((len(batch), self._model.num_states))
-		y = np.zeros((len(batch), self._model.num_actions))
+		x = np.zeros((len(batch), self._model._num_states))
+		y = np.zeros((len(batch), self._model._num_actions))
 		for i, b in enumerate(batch):
 			state, action, reward, next_state = b[0], b[1], b[2], b[3]
 			# get the current q values for all actions in state
